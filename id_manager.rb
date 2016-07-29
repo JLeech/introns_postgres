c02c69d0165a2e2a5577eb8a2d996b15a6b43764
Denis Gorev <eveleech@gmail.com>
2016-07-29 19:58:01 +0300

    add chunk ids for id_manager

--- id_manager.rb
+++ id_manager.rb
@@ -10,17 +10,18 @@ class IdManager
     self.genes_id = -1
   end
 
-  def inc_and_get_id(queue_name)
-    send("#{queue_name}_id=", send("#{queue_name}_id") +  1)
-    return send("#{queue_name}_id")
+  def inc_and_get_id(queue)
+    queue_id = "#{queue}_id"
+    send("#{queue_id}=", send(queue_id) +  1)
+    return send(queue_id)
   end
 
   def add_and_get_chunk_of_ids(number_of_ids, queue)
-
-
+    queue_id = "#{queue}_id"
+    start_id = send(queue_id) + 1
+    ids = (start_id..(number_of_ids + start_id-1)).to_a
+    send("#{queue_id}=", start_id + number_of_ids-1)
+    return ids
   end
-
-  #TODO make methods for getting ids in range
-  # for chunk loads
 
 end
