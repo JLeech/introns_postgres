5bd75c1025efe678231258b3c12d9e04ae15c138
Denis Gorev <eveleech@gmail.com>
2016-07-29 20:01:26 +0300

    small refactoring

--- id_manager.rb
+++ id_manager.rb
@@ -12,16 +12,16 @@ class IdManager
 
   def inc_and_get_id(queue)
     queue_id = "#{queue}_id"
-    send("#{queue_id}=", send(queue_id) +  1)
+    send("#{queue_id}=", send(queue_id) + 1)
     return send(queue_id)
   end
 
   def add_and_get_chunk_of_ids(number_of_ids, queue)
     queue_id = "#{queue}_id"
     start_id = send(queue_id) + 1
-    ids = (start_id..(number_of_ids + start_id-1)).to_a
+    ids = (start_id..(number_of_ids + start_id-1))
     send("#{queue_id}=", start_id + number_of_ids-1)
-    return ids
+    return ids.to_a
   end
 
 end
